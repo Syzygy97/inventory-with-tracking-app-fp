c00a13e6612b5035827724a2d13cdea3cdd02e77
aimielmarvin <aimielmarvin26@gmail.com>
2023-03-01 14:31:07 +0800

    Added feature that automatically creates a deployment record after deploying an asset

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -1,2 +1,5 @@
 class Deployment < ApplicationRecord
+  validates :vehicle_name, :address, :quantity, :price, :deployment_date, :deployment_time, :status, presence: true
+
+  enum :status, {Outgoing: 0, Incoming: 1}
 end
